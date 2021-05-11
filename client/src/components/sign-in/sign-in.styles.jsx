67e511bcc425ac0761b9ad7a619ca39a6e70cb9c
Austin Hou <austin@austinhou.com>
2021-05-11 07:58:42 -0700

    Sign in and Sign up mobile support

--- client/src/components/sign-in/sign-in.styles.jsx
+++ client/src/components/sign-in/sign-in.styles.jsx
@@ -4,6 +4,14 @@ export const SignInContainer = styled.div`
 	width: 28rem;
 	display: flex;
 	flex-direction: column;
+
+	@media screen and (max-width: 800px) {
+		width: auto;
+
+		button {
+			padding: auto 2rem;
+		}
+	}
 `;
 
 export const SignInTitle = styled.h2`
@@ -13,4 +21,12 @@ export const SignInTitle = styled.h2`
 export const ButtonsBarContainer = styled.div`
 	display: flex;
 	justify-content: space-between;
+
+	@media screen and (max-width: 800px) {
+		flex-direction: column;
+
+		button {
+			margin-bottom: 1rem;
+		}
+	}
 `;
