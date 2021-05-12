7fbe6f547116c27cd77aa56750c33a60c6ea5e41
Austin Hou <austin@austinhou.com>
2021-05-12 06:08:31 -0700

    Service worker test

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
+import serviceWorker from './registerServiceWorker';
+
 import './index.css';
 import App from './App';
 
 import { store, persistor } from './redux/store';
+
+serviceWorker();
 
 ReactDOM.render(
 	<React.StrictMode>
